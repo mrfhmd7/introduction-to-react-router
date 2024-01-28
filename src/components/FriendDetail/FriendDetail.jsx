b2ef708af61da6f8ea1475bb3fda7d77c671c63d
mrfhmd7 <mrf.hmd7@gmail.com>
2024-01-28 20:41:50 +0600

    Friend details components added

---
+++ src/components/FriendDetail/FriendDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendDetail = () => {
+     return (
+          <div>
+               <h2>All about friend.</h2>
+          </div>
+     );
+};
+
+export default FriendDetail;
